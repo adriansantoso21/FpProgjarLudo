f3ffa0764a4c2f83c023dc0f1c1bac0c16ce0753
Adrian <mkadriansantoso@gmail.com>
2022-05-21 13:03:29 +0700

    add dice to UI

--- ui/gameUi.py
+++ ui/gameUi.py
@@ -1,9 +1,19 @@
 import pygame
 pygame.init()
 
-def setWindow():
+def setGlobalVariable():
     global screen
+    global dicePlayer1
+    global dicePlayer2
+    global dicePlayer3
+    global dicePlayer4
     screen = pygame.display.set_mode((1400, 700))
+    dicePlayer1 = 1
+    dicePlayer2 = 1
+    dicePlayer3 = 1
+    dicePlayer4 = 1
+
+def setWindow():
     pygame.display.set_caption("Ludo Board Game")
     iconLogo = pygame.image.load("../asset/img/gameIcon.jpg")
     pygame.display.set_icon(iconLogo)
@@ -39,6 +49,26 @@ def setPlayerNameEmail():
     screen.blit(player3, (860, 25))
     screen.blit(player4, (860, 650))
 
+def setDice():
+    player1Dice = "../asset/img/dice/dice" + str(dicePlayer1) + ".png"
+    player2Dice = "../asset/img/dice/dice" + str(dicePlayer2) + ".png"
+    player3Dice = "../asset/img/dice/dice" + str(dicePlayer3) + ".png"
+    player4Dice = "../asset/img/dice/dice" + str(dicePlayer4) + ".png"
+
+    player1DiceImg = pygame.image.load(player1Dice)
+    player1DiceImg = pygame.transform.scale(player1DiceImg, (60, 60))
+    player2DiceImg = pygame.image.load(player2Dice)
+    player2DiceImg = pygame.transform.scale(player2DiceImg, (60, 60))
+    player3DiceImg = pygame.image.load(player3Dice)
+    player3DiceImg = pygame.transform.scale(player3DiceImg, (60, 60))
+    player4DiceImg = pygame.image.load(player4Dice)
+    player4DiceImg = pygame.transform.scale(player4DiceImg, (60, 60))
+
+    screen.blit(player1DiceImg, (85, 430))
+    screen.blit(player2DiceImg, (85, 205))
+    screen.blit(player3DiceImg, (880, 205))
+    screen.blit(player4DiceImg, (880, 430))
+
 def setBoard():
     xPos = 210
     yPos = 50
@@ -46,12 +76,14 @@ def setBoard():
     ySize = 600
     pygame.draw.rect(screen, (255, 255, 255), (xPos, yPos, xSize, ySize))
 
+setGlobalVariable()
 setWindow()
 running = True
 while running:
     setBackground()
     setPlayerIcon()
     setPlayerNameEmail()
+    setDice()
     setBoard()
     for event in pygame.event.get():
         if event.type == pygame.QUIT: running = False
