d0d0e92603f68d2de049014ece90fd3afe31c570
Adrian <mkadriansantoso@gmail.com>
2022-05-22 18:40:15 +0700

    add playerNameEmail class and display the container to screen

--- ui/regisUi.py
+++ ui/regisUi.py
@@ -1,6 +1,19 @@
 import pygame
 pygame.init()
 
+class PlayerNameEmail:
+    def __init__(self, colorInactive, colorActive, nameRect, emailRect):
+        self.colorInactive = colorInactive
+        self.colorActive = colorActive
+        self.nameColor = colorInactive
+        self.emailColor = colorInactive
+        self.nameActive = False
+        self.emailActive = False
+        self.name = ""
+        self.email = ""
+        self.nameRect = nameRect
+        self.emailRect = emailRect
+
 def setWindow():
     pygame.display.set_caption("Ludo Board Game")
     iconLogo = pygame.image.load("../asset/img/gameIcon.jpg")
@@ -41,12 +54,56 @@ def setPlayerIcon():
     screen.blit(iconLogo3, (765, 165))
     screen.blit(iconLogo4, (1080, 165))
 
+def setPlayerNameAndEmail():
+    for player in players:
+        #set text for nameEmail
+        text = pygame.font.Font(None, 24)
+        name = text.render(player.name, True, textColor)
+        email = text.render(player.email, True, textColor)
+        #draw nameEmail container
+        pygame.draw.rect(screen, player.nameColor, player.nameRect)
+        pygame.draw.rect(screen, player.emailColor, player.emailRect)
+        #draw nameText and emailText
+        screen.blit(name, (player.nameRect.x + 57.5, player.nameRect.y + 17.5))
+        screen.blit(email, (player.emailRect.x + 57.5, player.emailRect.y + 17.5))
+    pygame.display.update()
+
+
 def main():
     #declare global variable
-    global gameRunning, screen, textColor
+    global gameRunning, screen, textColor, player1, player2, player3, player4, players
     gameRunning = True
     screen = pygame.display.set_mode((1450, 700))
     textColor = (255, 255, 255)
+    #blue
+    player1 = PlayerNameEmail(
+        (1, 86, 213),#colorInactive
+        (1, 61, 152),#colorActive
+        pygame.Rect(152.5, 420, 200, 40),#nameRect
+        pygame.Rect(152.5, 480, 200, 40)#emailRect
+    )
+    #yellow
+    player2 = PlayerNameEmail(
+        (255, 179, 34),
+        (204, 133, 0),
+        pygame.Rect(460, 420, 200, 40),
+        pygame.Rect(460, 480, 200, 40)
+    )
+    #green
+    player3 = PlayerNameEmail(
+        (2, 188, 111),
+        (2, 100, 59),
+        pygame.Rect(780, 420, 200, 40),
+        pygame.Rect(780, 480, 200, 40)
+    )
+    #red
+    player4 = PlayerNameEmail(
+        (232, 55, 76),
+        (183, 21, 40),
+        pygame.Rect(1090, 420, 200, 40),
+        pygame.Rect(1090, 480, 200, 40)
+    )
+    players = [player1, player2, player3, player4]
 
     while gameRunning:
         setWindow()
@@ -54,6 +111,7 @@ def main():
         setTitle()
         setPlayerTitle()
         setPlayerIcon()
+        setPlayerNameAndEmail()
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT: gameRunning = False
