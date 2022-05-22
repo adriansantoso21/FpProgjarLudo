7cba4f00e1bce109fdfe20f25d88f7355be0ee22
Adrian <mkadriansantoso@gmail.com>
2022-05-22 23:42:20 +0700

    refactor gameUi

--- ui/gameUi.py
+++ ui/gameUi.py
@@ -1,6 +1,16 @@
 import pygame
 pygame.init()
 
+class player:
+    def __init__(self, name, namePosition, email, iconLogo, iconPosition, dice, dicePosition):
+        self.name = name
+        self.namePosition = namePosition
+        self.email = email
+        self.iconLogo = iconLogo
+        self.iconPosition = iconPosition
+        self.dice = dice
+        self.dicePosition = dicePosition
+
 def setWindow():
     pygame.display.set_caption("Ludo Board Game")
     iconLogo = pygame.image.load("../asset/img/gameIcon.jpg")
@@ -11,52 +21,25 @@ def setBackground():
     background = pygame.transform.scale(background, (1450, 700))
     screen.blit(background, (0,0))
 
-def setPlayerIcon():
-    iconLogo1 = pygame.image.load("../asset/img/playerIcon/icon1.png")
-    iconLogo1 = pygame.transform.scale(iconLogo1, (175, 175))
-    iconLogo2 = pygame.image.load("../asset/img/playerIcon/icon2.png")
-    iconLogo2 = pygame.transform.scale(iconLogo2, (175, 175))
-    iconLogo3 = pygame.image.load("../asset/img/playerIcon/icon3.png")
-    iconLogo3 = pygame.transform.scale(iconLogo3, (175, 175))
-    iconLogo4 = pygame.image.load("../asset/img/playerIcon/icon4.png")
-    iconLogo4 = pygame.transform.scale(iconLogo4, (175, 175))
-    screen.blit(iconLogo1, (25, 480))
-    screen.blit(iconLogo2, (25, 40))
-    screen.blit(iconLogo3, (820, 40))
-    screen.blit(iconLogo4, (820, 480))
+def setPlayerComponent():
+    for player in players:
+        #setIcon
+        iconLogo = pygame.image.load(player.iconLogo)
+        iconLogo = pygame.transform.scale(iconLogo, (175, 175))
+        screen.blit(iconLogo, player.iconPosition)
 
-def setPlayerNameEmail():
-    font = pygame.font.Font('freesansbold.ttf', 24)
-    player1 = font.render(playerName1, True, textColor)
-    player2 = font.render(playerName2, True, textColor)
-    player3 = font.render(playerName3, True, textColor)
-    player4 = font.render(playerName4, True, textColor)
-    screen.blit(player1, (65, 650))
-    screen.blit(player2, (65, 25))
-    screen.blit(player3, (860, 25))
-    screen.blit(player4, (860, 650))
+        #setName
+        font = pygame.font.Font('freesansbold.ttf', 24)
+        name = font.render(player.name, True, textColor)
+        screen.blit(name, player.namePosition)
 
-def setDice():
-    player1Dice = "../asset/img/dice/dice" + str(dicePlayer1) + ".png"
-    player2Dice = "../asset/img/dice/dice" + str(dicePlayer2) + ".png"
-    player3Dice = "../asset/img/dice/dice" + str(dicePlayer3) + ".png"
-    player4Dice = "../asset/img/dice/dice" + str(dicePlayer4) + ".png"
+        #setDice
+        dice = "../asset/img/dice/dice" + str(player.dice) + ".png"
+        dice = pygame.image.load(dice)
+        dice = pygame.transform.scale(dice, (60,60))
+        screen.blit(dice, player.dicePosition)
 
-    player1DiceImg = pygame.image.load(player1Dice)
-    player1DiceImg = pygame.transform.scale(player1DiceImg, (60, 60))
-    player2DiceImg = pygame.image.load(player2Dice)
-    player2DiceImg = pygame.transform.scale(player2DiceImg, (60, 60))
-    player3DiceImg = pygame.image.load(player3Dice)
-    player3DiceImg = pygame.transform.scale(player3DiceImg, (60, 60))
-    player4DiceImg = pygame.image.load(player4Dice)
-    player4DiceImg = pygame.transform.scale(player4DiceImg, (60, 60))
-
-    screen.blit(player1DiceImg, (85, 430))
-    screen.blit(player2DiceImg, (85, 205))
-    screen.blit(player3DiceImg, (880, 205))
-    screen.blit(player4DiceImg, (880, 430))
-
-    rollDiceButton = pygame.Rect(1115, 595, 200, 50)
+def setRollDiceButton():
     buttonColor = (232, 55, 76)
     pygame.draw.rect(screen, buttonColor, rollDiceButton, border_radius = 25)
 
@@ -90,12 +73,12 @@ def setChatBox():
 
 def main(playerNameEmail):
     #declare global variable
-    global gameRunning, screen, dicePlayer1, dicePlayer2, dicePlayer3, dicePlayer4, textColor, colorInactive
+    global gameRunning, screen, rollDiceButton, textColor, colorInactive
     global colorActive, textAreaColor, textAreaActive, textChat, textAreaRect
-    global playerName1, playerEmail1, playerName2, playerEmail2, playerName3, playerEmail3, playerName4, playerEmail4
+    global firstTime, playerOrder, player1, player2, player3, player4, players
     gameRunning = True
     screen = pygame.display.set_mode((1450, 700))
-    dicePlayer1 = dicePlayer2 = dicePlayer3 = dicePlayer4 = 1
+    rollDiceButton = pygame.Rect(1115, 595, 200, 50)
     textColor = (255, 255, 255)
     colorInactive = (156, 121, 99)
     colorActive = (93, 73, 60)
@@ -103,17 +86,51 @@ def main(playerNameEmail):
     textAreaActive = False
     textChat = ''
     textAreaRect = pygame.Rect(1040, 515, 350, 50)
-    playerName1 = playerNameEmail[0]; playerEmail1 = playerNameEmail[1]
-    playerName2 = playerNameEmail[2]; playerEmail2 = playerNameEmail[3]
-    playerName3 = playerNameEmail[4]; playerEmail3 = playerNameEmail[5]
-    playerName4 = playerNameEmail[6]; playerEmail4 = playerNameEmail[7]
+    firstTime = True
+    playerOrder = []
+    player1 = player(
+        playerNameEmail[0],
+        (65, 650),
+        playerNameEmail[1],
+        "../asset/img/playerIcon/icon1.png",
+        (25, 480),
+        1,
+        (85, 430)
+    )
+    player2 = player(
+        playerNameEmail[2],
+        (65, 25),
+        playerNameEmail[3],
+        "../asset/img/playerIcon/icon2.png",
+        (25, 40),
+        1,
+        (85, 205)
+    )
+    player3 = player(
+        playerNameEmail[4],
+        (860, 25),
+        playerNameEmail[5],
+        "../asset/img/playerIcon/icon3.png",
+        (820, 40),
+        1,
+        (880, 205)
+    )
+    player4 = player(
+        playerNameEmail[6],
+        (860, 650),
+        playerNameEmail[7],
+        "../asset/img/playerIcon/icon4.png",
+        (820, 480),
+        1,
+        (880, 430)
+    )
+    players = [player1, player2, player3, player4]
 
     while gameRunning:
         setWindow()
         setBackground()
-        setPlayerIcon()
-        setPlayerNameEmail()
-        setDice()
+        setPlayerComponent()
+        setRollDiceButton()
         setBoard()
         setChatBox()
 
@@ -129,9 +146,7 @@ def main(playerNameEmail):
             if event.type == pygame.KEYDOWN:
                 if textAreaActive:
                     if event.key == pygame.K_RETURN:
-                        print(textChat)
                         textChat = ""
-                        # TODO: add the logic soon
                     elif event.key == pygame.K_BACKSPACE:
                         textChat = textChat[:-1]
                     else:
