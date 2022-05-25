ebda8e2e4f8586cd5966fe56d38f5d6d35384d89
Adrian <mkadriansantoso@gmail.com>
2022-05-26 01:14:19 +0700

    fix ruleWinConditionData

--- entity/ruleWinCondition.py
+++ entity/ruleWinCondition.py
@@ -1,7 +1,6 @@
 class RuleWinCondition:
-    def __init__(self, text, texPosition, image, imagePosition, container):
+    def __init__(self, text, image, imagePosition, container):
         self.text = text
-        self.texPosition = texPosition
         self.image = image
         self.imagePosition = imagePosition
         self.container = container
