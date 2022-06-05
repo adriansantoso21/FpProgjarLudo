fef8d6c1ec821f48e15b05e2c56e696726cc5407
Adrian <mkadriansantoso@gmail.com>
2022-06-05 21:29:52 +0700

    change smtp to gmail

--- logic/shareEmail.py
+++ logic/shareEmail.py
@@ -2,20 +2,24 @@ import smtplib
 
 def main(playerEmail, position):
    to = playerEmail
-   sender = 'ludoprogjar@yahoo.com'
-   password = 'ixfmtzqzsbbhcpeu'
+   sender = 'ludoboardgamefp@gmail.com'
+   subject = 'Congratulations Message'
+   password = 'xxx'
 
-   message = "Congratulations, you have finished in " + position + " place. Hope you enjoy the game :)"
-
+   text = "Congratulations, you have finished in " + \
+          position + \
+          " place. Don't forget to play with your frieds again. Hope you enjoy the game.\nBest Regard,\nLudo Board Game Team"
+   
+   message = """From: %s\nTo: %s\nSubject: %s\n\n%s
+       """ % (sender, ", ".join(playerEmail), subject, text)
    try:
-      server = smtplib.SMTP_SSL("smtp.mail.yahoo.com", 465)
-      # server.connect("smtp.mail.yahoo.com", 465)
-      # server.ehlo()
-      # server.starttls()
+      server = smtplib.SMTP("smtp.gmail.com", 587)
       server.ehlo()
+      server.starttls()
       server.login(sender, password)
       server.sendmail(sender, to, str(message))
-      server.quit()
+      server.close()
+      print("Successfull send email")
 
    except Exception as err:
          print(err)
